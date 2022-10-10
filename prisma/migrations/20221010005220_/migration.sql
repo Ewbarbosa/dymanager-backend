dfbd6567c8e21ae783dbed0360ee1e5dfade507d
Ewerton Barbosa <ewertonb@live.com>
2022-10-09 22:09:48 -0300

    Novas tabelas criadas

---
+++ prisma/migrations/20221010005220_/migration.sql
@@ -0,0 +1,124 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `users` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "cnpjcpf" TEXT NOT NULL,
+    "sex" TEXT NOT NULL,
+    "nationality" TEXT NOT NULL,
+    "born_in" TIMESTAMP(3) NOT NULL,
+    "telephone" TEXT NOT NULL,
+    "telephone2" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "office" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "address" (
+    "id" SERIAL NOT NULL,
+    "street" TEXT NOT NULL,
+    "zip_code" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "client_id" INTEGER NOT NULL,
+
+    CONSTRAINT "address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "calls" (
+    "id" SERIAL NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "end" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "contact_channel" TEXT NOT NULL,
+    "client_id" INTEGER NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "calls_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "loans" (
+    "id" SERIAL NOT NULL,
+    "n_installments" INTEGER NOT NULL,
+    "v_installments" DOUBLE PRECISION NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "start" TIMESTAMP(3) NOT NULL,
+    "end" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "category" TEXT NOT NULL,
+    "bank_account_id" INTEGER NOT NULL,
+    "call_id" INTEGER NOT NULL,
+    "client_id" INTEGER NOT NULL,
+
+    CONSTRAINT "loans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "banks" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "code" INTEGER NOT NULL,
+
+    CONSTRAINT "banks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bank_account" (
+    "id" SERIAL NOT NULL,
+    "bank_id" INTEGER NOT NULL,
+    "agency" INTEGER NOT NULL,
+    "account" INTEGER NOT NULL,
+    "client_id" INTEGER NOT NULL,
+
+    CONSTRAINT "bank_account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_cnpjcpf_key" ON "clients"("cnpjcpf");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "loans_bank_account_id_key" ON "loans"("bank_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "loans_call_id_key" ON "loans"("call_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "address" ADD CONSTRAINT "address_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "calls" ADD CONSTRAINT "calls_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "calls" ADD CONSTRAINT "calls_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loans" ADD CONSTRAINT "loans_bank_account_id_fkey" FOREIGN KEY ("bank_account_id") REFERENCES "bank_account"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loans" ADD CONSTRAINT "loans_call_id_fkey" FOREIGN KEY ("call_id") REFERENCES "calls"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "loans" ADD CONSTRAINT "loans_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_bank_id_fkey" FOREIGN KEY ("bank_id") REFERENCES "banks"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bank_account" ADD CONSTRAINT "bank_account_client_id_fkey" FOREIGN KEY ("client_id") REFERENCES "clients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
